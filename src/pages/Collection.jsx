004e0c506a82e26b611c94a861d4720ecb07085e
Elly <khuonghailam00@gmail.com>
2025-10-18 17:46:52 +0700

    temporary for Collection

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/shopContext";
 
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filtering */}
+      <div className="min-w-60">
+        <p
+          className="my-2 text-x1 flex items-center cursor-pointer gap-2"
+          onClick={() => setShowFilter(!showFilter)}
+        >
+          Filter
+        </p>
+        <div
+          className={`border-gray-300 border pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
       <BestSeller></BestSeller>
       <Policy></Policy>
       <News></News>
+      
 
     </div>
   );
