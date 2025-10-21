62200ae99f3ae2ab12b4207da76c3953f632655f
Elly <khuonghailam00@gmail.com>
2025-10-22 00:19:34 +0700

    Add PAYMENT node (not work yet, just scaffolding)

---
+++ payment-service/server.js
@@ -0,0 +1,31 @@
+// environment
+import dotenv from 'dotenv';
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+dotenv.config({ path: envFile });
+
+
+//tools
+import express from 'express';
+import bodyParser from 'body-parser';
+
+//modules
+import connectDB from './config/db.js'
+
+//routing modules
+// import userMeRoutes from './routes/users/users-me.js';
+
+
+//initialisation
+
+const app = express();
+const PORT = process.env.PORT || 6003;
+connectDB();
+
+// use json for the whole application, this automatically parse JSON into objects
+app.use(bodyParser.json()); 
+
+// Routes listing
+
+
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
