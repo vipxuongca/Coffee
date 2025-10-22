fb72ab494a5b579b391a552f7090a66c81898839
Elly <khuonghailam00@gmail.com>
2025-10-22 12:25:08 +0700

    change git ignore to track assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 
 const App = () => {
-  return <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+  return <div className="px-4 lg:px-[9vw]">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
