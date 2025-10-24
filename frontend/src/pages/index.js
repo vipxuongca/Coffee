a427776bb596e318f7d3df8ea32075cb5379a2b8
Elly <khuonghailam00@gmail.com>
2025-10-24 18:11:10 +0700

    minor front end change

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,11 @@
+export { default as Home } from "./Home.jsx";
+export { default as About } from "./About.jsx";
+export { default as Contact } from "./Contact.jsx";
+export { default as Collection } from "./Collection.jsx";
+export { default as Product } from "./Product.jsx";
+export { default as Cart } from "./Cart.jsx";
+export { default as Login } from "./Login.jsx";
+export { default as PlaceOrder } from "./PlaceOrder.jsx";
+export { default as Order } from "./Order.jsx";
+export { default as Register } from "./Register.jsx";
+export { default as UserDashboard } from "./UserDashboard.jsx";
