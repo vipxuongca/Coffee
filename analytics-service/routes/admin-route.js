d28ba865539642d1e9239e6aafadd224203d3b18
Elly <khuonghailam00@gmail.com>
2025-11-07 17:48:24 +0700

    remove analytic service. Merge to admin instead

--- analytics-service/routes/admin-route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginAdmin, registerAdmin } from '../controllers/admin-controller.js';
-
-const router = express.Router();
-
-router.post('/login', loginAdmin);
-router.post('/register', registerAdmin);
-
-export default router;
