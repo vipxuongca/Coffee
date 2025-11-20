d498ad55851b30597e5d75e983a35556669ea49c
Elly <khuonghailam00@gmail.com>
2025-11-21 00:10:27 +0700

    working cart

--- cart-service/config/api.js
+++ cart-service/config/api.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+dotenv.config({ path: envFile });
+
 // api/product.js
-const BASE_PRODUCT_URL = 'http://localhost:4000';
+const BASE_PRODUCT = process.env.API_PRODUCT;
 
 export const productApi = {
-  delete: `${BASE_PRODUCT_URL}/api/product/delete`,
-  list: `${BASE_PRODUCT_URL}/api/product`,
-  create: `${BASE_PRODUCT_URL}/api/product/create`,
-  getOneProduct: ` ${BASE_PRODUCT_URL}/api/product/fetch`//with id passed by the controller
+  delete: `${BASE_PRODUCT}/api/product/delete`,
+  list: `${BASE_PRODUCT}/api/product`,
+  create: `${BASE_PRODUCT}/api/product/create`,
+  getOneProduct: `${BASE_PRODUCT}/api/product/fetch`//with id passed by the controller
 };
