61d25034ae7607a237803e02e96ea83e8637c513
Elly <khuonghailam00@gmail.com>
2025-11-22 05:56:39 +0700

    Adjust logins: empty the memory after log in

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -17,6 +17,8 @@ const Login = () => {
 
       if (response.data.success) {
         setToken(response.data.token);
+        setEmail("");
+        setPassword("");
         toast.success("Đăng nhập Admin thành công");
       } else {
         toast.error(response.data.message);
@@ -30,7 +32,7 @@ const Login = () => {
   };
 
   return (
-     <div className="min-h-screen flex items-center justify-center bg-[#f8f3ef] px-4">
+    <div className="min-h-screen flex items-center justify-center bg-[#f8f3ef] px-4">
       <div className="bg-[#fff8f0] border border-[#d7ccc8]   shadow-inner w-full max-w-md p-8">
         <h1 className="text-2xl font-bold text-center text-[#3e2723] mb-6 border-b border-[#a1887f] pb-2">
           ĐĂNG NHẬP ADMIN
@@ -71,7 +73,6 @@ const Login = () => {
           >
             Đăng nhập
           </button>
-
         </form>
       </div>
     </div>
