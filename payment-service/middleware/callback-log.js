d132e67a7aec73d20dad57c516e2849ab9738039
Elly <khuonghailam00@gmail.com>
2026-01-25 20:09:21 +0700

    add middleware for debug

---
+++ payment-service/middleware/callback-log.js
@@ -0,0 +1,4 @@
+const callbackLog = (req, res, next) => {
+  console.log('Callback Log Middleware Reached:', req.method, req.originalUrl, req.body);
+  next();
+}
--- payment-service/routes/momo-route.js
+++ payment-service/routes/momo-route.js
@@ -3,12 +3,13 @@ import express from "express";
 // import CartModel from "../models/cart-model.js";
 import { momoClient, momoCallback, momoVerifyTransaction } from "../controllers/momo-controller.js";
 import { verifyToken } from "../middleware/jwt-verify.js";
+import { callbackLog } from "../middleware/callback-log.js";
 
 const router = express.Router();
 // 4008/api/momo
 
 router.post("/create", momoClient);
-router.post("/callback", momoCallback);
+router.post("/callback", callbackLog, momoCallback);
 router.post("/verify-transaction", momoVerifyTransaction);
 
 
